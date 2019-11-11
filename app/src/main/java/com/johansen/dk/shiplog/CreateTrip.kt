0528ea7d44cb9680eed2cccce72c8ad8fafc97e8
Simon J <Neelix548@hotmail.com>
2019-11-11 23:38:43 +0100

    Removed functions with too high Android requirements.

--- app/src/main/java/com/johansen/dk/shiplog/CreateTrip.kt
+++ app/src/main/java/com/johansen/dk/shiplog/CreateTrip.kt
@@ -2,23 +2,13 @@ package com.johansen.dk.shiplog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_create_trip.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+
 
 class CreateTrip : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_trip)
-
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-        val formatted = current.format(formatter)
-
-        testText.text = formatted
-
-
 
     }
 }
